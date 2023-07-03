0a50e9fa5ef776b4f1313d9cd7adc989410d4017
k-ardliyan <k.ardliyan@gmail.com>
2023-07-03 18:03:52 +0700

    menambahkan fungsi print

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿class Program
 {
+    static void Print(message)
+    {
+        Console.WriteLine("\n" + message);
+    }
+    
     static void Main()
     {
         Console.WriteLine("hallo kang!!!!");
+        Print("hallo juga");
     }
 }
